b8fb8942c514188e55f20a7ce90628fa53437eda
leticiaoliv <43250283+leticiaoliv@users.noreply.github.com>
2018-10-01 19:24:12 -0300

    Update integral.jl
    Versão 2

--- integral.jl
+++ integral.jl
@@ -15,11 +15,10 @@ function simpsonrep(f::Function, a::Real, b::Real; m = 101)
     end
     I = h/3 * (f(a) + 4 * I1 + 2 * I2 + f(b))
     return I
-    return 0.0
 end
 
 
-function simpsoneps(f, a, b, ϵ; M = 1.0)
+function simpsoneps(f::Function, a::Real, b::Real, ϵ::Real; M = 1.0)
     h = (((180 * ϵ )/((b - a) * M))^(0.25))
     m = ceil(Int, (h / (b - a)) - 1)
     return simpsonrep(f, a, b, m)
@@ -27,8 +26,7 @@ end
 
 # II
 function simpson(f::Function, a::Real, b::Real)
-    h = (b - a) / 2
-    return h * (f(a) + 4 * f(a + h) + f(b)) / 3
+    return (b - a) * (f(a) + 4 * f((a + b)/2) + f(b)) / 6
 end
 
 function simpson_adaptivo(f::Function, a::Real, b::Real, ϵ::Real)
@@ -40,7 +38,7 @@ function simpson_adaptivo_recursivo(f::Function, a::Real, b::Real, ϵ::Real, I::
     c = (a + b) / 2
     esquerda = simpson(f, a, c)
     direita = simpson(f, c, b)
-    if abs(I - esquerda - direita) <= (15 * ϵ) 
+    if abs(I - esquerda - direita) <= (15 * ϵ)
         return (esquerda + direita)
     end
     return (simpson_adaptivo_recursivo(f, a, b, ϵ/2, esquerda) + simpson_adaptivo_recursivo(f, a, b, ϵ/2, direita))
